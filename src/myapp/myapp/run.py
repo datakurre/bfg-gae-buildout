78a07746fb1c69ac73d6ded98f76fc76bb3dd425
datakurre <datakurre@e2554e72-13cc-11df-a685-e9edad190965>
2011-06-21 08:39:53 +0000

    Updated to to use pyramid.config instead of deprecated pyramid.configurator.

--- src/myapp/myapp/run.py
+++ src/myapp/myapp/run.py
@@ -3,12 +3,12 @@
 
 import pyramid_zcml
 
-from pyramid.configuration import Configurator
+from pyramid.config import Configurator
 from myapp.app import get_root
 
 
 def wsgi_app(**settings):
-    config = Configurator(root_factory=get_root, settings=settings)
+    config = Configurator(root_factory=get_root, settings=settings, autocommit=True)
     config.include(pyramid_zcml)
     config.load_zcml("configure.zcml")
     return config.make_wsgi_app()
